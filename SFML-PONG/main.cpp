74a5de960ca812776c8be6636b14362bb016cdd7
BBBomber <yashasvi.chopra555@gmail.com>
2024-09-12 11:49:54 +0530

    code to open a window
    800 by 600 window with the default options

--- SFML-PONG/main.cpp
+++ SFML-PONG/main.cpp
@@ -1,7 +1,28 @@
-
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    // Create a window with dimensions 800x600 and title "Pong"
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
+
+    // Main game loop: runs as long as the window is open
+    while (window.isOpen())
+    {
+        // Event handling
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // If the close button is pressed, close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear the window with a black color
+        window.clear(sf::Color::Black);
+
+        // Display what has been drawn to the window
+        window.display();
+    }
 
     return 0;
 }
