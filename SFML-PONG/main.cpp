393ce0fc0fcba2fcea60b9d9b850c4406fb9dbbc
BBBomber <yashasvi.chopra555@gmail.com>
2024-09-12 13:30:28 +0530

    Aspect ratio
    function maintain aspect ratio when going fullscreen
    
    sfml automatically scales stuff but we needed a fucntion to maintain the aspect ratio when changing screen size
    
    new concepts: view + viewport + resized event + aspect ratio

--- SFML-PONG/main.cpp
+++ SFML-PONG/main.cpp
@@ -3,49 +3,76 @@
 // Function to create and return a rectangle shape
 sf::RectangleShape createRectangle(float width, float height, sf::Vector2f position, sf::Color fillColor, sf::Color outlineColor, float outlineThickness)
 {
-    // Create a rectangle shape with the specified dimensions
     sf::RectangleShape rectangle(sf::Vector2f(width, height));
-
-    // Set the position of the rectangle
     rectangle.setPosition(position);
-
-    // Set the color of the rectangle (fill color)
     rectangle.setFillColor(fillColor);
-
-    // Set the outline color and thickness
     rectangle.setOutlineColor(outlineColor);
     rectangle.setOutlineThickness(outlineThickness);
 
     return rectangle;
 }
 
+// Function to maintain the aspect ratio when the window is resized
+void maintainAspectRatio(sf::RenderWindow& window, sf::View& view, float aspectRatio)
+{
+    // Get the new window size
+    sf::Vector2u windowSize = window.getSize();
+
+    // Calculate the new aspect ratio
+    float windowAspectRatio = static_cast<float>(windowSize.x) / static_cast<float>(windowSize.y);
+
+    // Determine if we need to add letterboxing (black bars) on the sides or top/bottom
+    if (windowAspectRatio > aspectRatio) {
+        // Window is wider than the desired aspect ratio, add black bars on the sides
+        float newWidth = aspectRatio / windowAspectRatio;
+        view.setViewport(sf::FloatRect((1.f - newWidth) / 2.f, 0.f, newWidth, 1.f));
+    }
+    else {
+        // Window is taller than the desired aspect ratio
+        float newHeight = windowAspectRatio / aspectRatio;
+        view.setViewport(sf::FloatRect(0.f, (1.f - newHeight) / 2.f, 1.f, newHeight));
+    }
+
+    // Apply the view with the correct aspect ratio
+    window.setView(view);
+}
+
 int main()
 {
-    // Create a window with dimensions 800x600 and title "Pong"
+
     sf::RenderWindow window(sf::VideoMode(800, 600), "Pong");
 
-    // Create a rectangle with 100x50 size, position it at (350, 275), set fill color, outline color, and thickness
-    sf::RectangleShape rectangle = createRectangle(100, 50, sf::Vector2f(350, 275), sf::Color::Red, sf::Color::Yellow, 5.f);
+    //changed the x location to show the fullscreen maintained aspect ratio
+    sf::RectangleShape rectangle = createRectangle(100, 50, sf::Vector2f(700, 275), sf::Color::Red, sf::Color::Green, 5.f);
 
-    // Main game loop: runs as long as the window is open
+    // Create a view with the same size as the original window
+    sf::View view(sf::FloatRect(0, 0, 800, 600)); //think of this to be a sort of camera
+
+    // Aspect ratio of 800x600 is 4:3
+    float aspectRatio = 4.f / 3.f;
+
     while (window.isOpen())
     {
-        // Event handling
+   
         sf::Event event;
         while (window.pollEvent(event))
         {
             // If the close button is pressed, close the window
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Handle window resizing
+            if (event.type == sf::Event::Resized)
+            {
+                // Maintain the aspect ratio when the window is resized
+                maintainAspectRatio(window, view, aspectRatio); //comment this and go fullscreen to see the difference
+            }
         }
 
-        // Clear the window with a black color
-        window.clear(sf::Color::Black);
+        window.clear(sf::Color::Black); 
 
-        // Draw the rectangle with the outline
+        // Draw the rectangle with the correct aspect ratio
         window.draw(rectangle);
-
-        // Display what has been drawn to the window
         window.display();
     }
 
